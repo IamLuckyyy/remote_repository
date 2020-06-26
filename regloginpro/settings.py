7100bc42f213890b0fc14c8876f75ab40c1747ef
sdoralucky1912 <sdoralucky1912@gmail.com>
2020-06-26 12:14:28 +0530

    firstcommit

--- regloginpro/settings.py
+++ regloginpro/settings.py
@@ -123,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
